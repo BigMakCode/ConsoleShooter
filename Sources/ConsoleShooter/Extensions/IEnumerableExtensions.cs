9a994b9a3e5ef620f332664e2198f09a99941c08
BigMakCode <81349914+BigMakCode@users.noreply.github.com>
2023-09-12 15:50:07 -0600

    Fix random using

--- Sources/ConsoleShooter/Extensions/IEnumerableExtensions.cs
+++ Sources/ConsoleShooter/Extensions/IEnumerableExtensions.cs
@@ -4,7 +4,7 @@
     {
         public static T RandomElement<T>(this IEnumerable<T> enumerable)
         {
-            return enumerable.RandomElementUsing<T>(new Random());
+            return enumerable.RandomElementUsing<T>(Random.Shared);
         }
 
         public static T RandomElementUsing<T>(this IEnumerable<T> enumerable, Random rand)
